7de3b1f1a71471f62c65c70007651e2742c5fb50
Johnny Svensson <Johnny.Svensson15@gmail.com>
2018-09-03 10:57:26 +0200

    Not sure what I've added

---
+++ src/FrenchRules.js
@@ -0,0 +1,12 @@
+var jsonObject = {
+    Words:{Letters:[
+        {cc:[
+            {description:"if before e, it will sound like X", pronoun:"Think of it like an axe"}]},
+        {j:[
+            {description:"if simple word like 'je'", pronoun:"Jöööööööö"}
+        ]}
+        ]}
+}
+
+export default jsonObject;
+
